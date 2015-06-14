21e61953a3137dca073d3d5ece616001ebe7941b
sumotoy <sumotoy@gmail.com>
2015-06-14 17:13:03 +0200

    fix a problem with fillRect and 8bit CPU
    Affected also font rendering

--- TFT_ILI9163C.cpp
+++ TFT_ILI9163C.cpp
@@ -905,6 +905,8 @@ void TFT_ILI9163C::fillRect(int16_t x, int16_t y, int16_t w, int16_t h, uint16_t
 		}
 		#if defined(__MK20DX128__) || defined(__MK20DX256__)
 			writedata16_last(color);
+		#else
+			writedata16(color);
 		#endif
 	}
 	#if defined(SPI_HAS_TRANSACTION)
